3a9cab645734dfee3be70560c1e62ca407db4aa6
scsknghtblde <scsknghtblde@gmail.com>
2016-08-10 13:51:37 -0400

    If Number MOD 3 and MOD 5 Change number to Mined Mines

--- Number_Increment.rb
+++ Number_Increment.rb
@@ -1,14 +1,15 @@
 number=0
 100.times do
 	number = number+1
-		if number % 3 == && number % 3 ==0
+		if number % 3 ==0 && number % 5 ==0
 				puts "Mined Minds"
-		elsif number % 3==0
+		elsif 
+			number % 3==0
 			 	puts "Mined"
 		elsif
 		 	number % 5==0
 			 	puts "Minds"
 		else
 			puts number
-	end
+		end
 end

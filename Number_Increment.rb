58f1ba8ebaabc1a54c078d77052caf9d2237ce06
scsknghtblde <scsknghtblde@gmail.com>
2016-08-10 13:26:12 -0400

    Replace 3 with mined 5 with mind

--- Number_Increment.rb
+++ Number_Increment.rb
@@ -1,5 +1,15 @@
-number=1
-10.times do
-	puts number
-	number = number+1	
+number=0
+100.times do
+	number = number+1
+		if number==3
+			 	puts "Mined"
+		elsif
+			number==5
+			 	puts "Minds"
+		else
+			puts number
+		
+	end
+		
+
 end

37ccbc1eef450c86ec5e867c3c3be49a847aa5f1
scsknghtblde <scsknghtblde@gmail.com>
2016-08-15 08:59:08 -0400

    Add files via upload

--- number_incrementv1.rb
+++ number_incrementv1.rb
@@ -1,12 +1,17 @@
-def mined_minds(number)
-	if number % 3==0 && number %5==0
-		"mined_minds"
+def mined_minds()
+	numbers = [*0..9999] 										#defines and populates array
+	if numbers.each_with_index do |value,index|					#creates the index for the array
+		if value %3==0 && value %5==0							#check if value is div by 5
+			numbers[index]="mined_minds"						#If affirmative, changes value to "mined_minds"
 
-	elsif number %3==0
-		"mined"
-	elsif number %5==0
-		"minds"
-	else
-		 number
-	end
+		elsif value %3==0										#check if value is div by 3
+			numbers[index]="mined"								#if affirmative, changes value to "mined"
+				
+		else value %5==0										#check if value is div by 5
+			numbers[index]="minds"								#if affirmative, changes value to "mined"
+				
+		end 													#End if
+		end 													#End do
+	end 														#End def
+numbers
 end
